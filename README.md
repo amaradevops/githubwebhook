ff0ac2544caf29c693420fef20e906e920886bec
amaradevops <amaradevops1234@gmail.com>
2019-08-27 15:44:59 +0530

    addding inde

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > A GitHub App built with [Probot](https://github.com/probot/probot) that A Probot App
 
-## Setup
+# Setup
 
 ```sh
 # Install dependencies
@@ -12,12 +12,12 @@ npm install
 npm start
 ```
 
-## Contributing
+# Contributing
 
 If you have suggestions for how githubwebhook could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
 
 For more, check out the [Contributing Guide](CONTRIBUTING.md).
 
-## License
+# License
 
 [ISC](LICENSE) © 2019 amaradevops <amaradevops1234@gmail.com> (https://github.com/amaradevops/githubwebhook)
--- index.js
+++ index.js
@@ -2,18 +2,14 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
-module.exports = app => {
+ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+  robot.on('pull_request.opened',test)
+   async function test (context)  {
+     // leave comment
+    const param = context.issue({ body: 'hello world' })
+    await context.github.issues.createComment(param)
+  }
+ }
